89e916f3ae3f83530282ba0109ec47497e4dfb35
PrimeO7 <nowak.philipp97@gmail.com>
2023-02-09 20:33:22 +0100

    fix(nvim): move text up and down

--- home/.config/nvim/lua/user/keymaps.lua
+++ home/.config/nvim/lua/user/keymaps.lua
@@ -47,3 +47,10 @@ keymap("v", ">", ">gv", opts)
 keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
