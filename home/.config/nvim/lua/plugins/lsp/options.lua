722202002ce7f2f3cd06eaa62ed8c85b9ab0c417
PrimeO7 <nowak.philipp97@gmail.com>
2023-04-21 22:18:28 +0200

    style(nvim): formatting

--- home/.config/nvim/lua/plugins/lsp/options.lua
+++ home/.config/nvim/lua/plugins/lsp/options.lua
@@ -6,27 +6,29 @@ local M = {}
 -- after the language server attaches to the current buffer
 M.on_attach = function(client, bufnr)
 	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
 	local function normal_map(keys, func, desc)
 		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
 	end
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	normal_map('gD', vim.lsp.buf.declaration, "Go to Declaration")
-	normal_map('gd', vim.lsp.buf.definition, "Go to Definition")
-	normal_map('K', vim.lsp.buf.hover, "Show Documentation")
-	normal_map('gi', vim.lsp.buf.implementation, "Go to implementation")
-	normal_map('<C-k>', vim.lsp.buf.signature_help, "Signature Documentation")
-	normal_map('<leader>wa', vim.lsp.buf.add_workspace_folder, "Workspace add folder")
-	normal_map('<leader>wr', vim.lsp.buf.remove_workspace_folder, "Workspace remove folder")
-	normal_map('<leader>wl', function()
+	normal_map("gD", vim.lsp.buf.declaration, "Go to Declaration")
+	normal_map("gd", vim.lsp.buf.definition, "Go to Definition")
+	normal_map("K", vim.lsp.buf.hover, "Show Documentation")
+	normal_map("gi", vim.lsp.buf.implementation, "Go to implementation")
+	normal_map("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+	normal_map("<leader>wa", vim.lsp.buf.add_workspace_folder, "Workspace add folder")
+	normal_map("<leader>wr", vim.lsp.buf.remove_workspace_folder, "Workspace remove folder")
+	normal_map("<leader>wl", function()
 		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 	end, "Workspace list folders")
-	normal_map('<leader>D', vim.lsp.buf.type_definition, "Go to Type_definition")
-	normal_map('<leader>rn', vim.lsp.buf.rename, "Rename")
-	normal_map('<leader>ca', vim.lsp.buf.code_action, "Code action")
-	normal_map('gr', vim.lsp.buf.references, "List refrences")
-	normal_map('<leader>n', function() vim.lsp.buf.format { async = true } end, "Autoformat file")
+	normal_map("<leader>n", function()
+		vim.lsp.buf.format({ async = true })
+	end, "Autoformat file")
+	normal_map("<leader>D", vim.lsp.buf.type_definition, "Go to Type_definition")
+	normal_map("<leader>rn", vim.lsp.buf.rename, "Rename")
+	normal_map("<leader>ca", vim.lsp.buf.code_action, "Code action")
+	normal_map("gr", vim.lsp.buf.references, "List refrences")
 end
 
 M.lsp_flags = {
@@ -42,9 +44,8 @@ end
 
 M.capabilities = cmp_nvim_lsp.default_capabilities()
 
-
-vim.diagnostic.config {
+vim.diagnostic.config({
 	virtual_text = false,
-}
+})
 
 return M
