944c8a04f74b2c41356372537be57919f5bddd01
PrimeO7 <nowak.philipp97@gmail.com>
2023-04-24 19:44:57 +0200

    feat(nvim): suppress warning from clangd lsp
    suppress warning form clangd lsp when mutliple lsp are attached to
    buffer

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,3 +1,12 @@
 require "user.options"
 require "user.keymaps"
 require "user.lazy"
+
+local notify = vim.notify
+vim.notify = function(msg, ...)
+    if msg:match("warning: multiple different client offset_encodings") then
+        return
+    end
+
+    notify(msg, ...)
+end
